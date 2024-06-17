6875931875a229666424366c69c531477bfe9147
Badal Kumar <bk82623@gmail.com>
2024-06-17 23:57:59 +0530

    init commit

--- 09.Smart-Contact-Manager/src/main/java/com/scm/controller/UseController.java
+++ 09.Smart-Contact-Manager/src/main/java/com/scm/controller/UseController.java
@@ -1,7 +1,18 @@
 package com.scm.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class UseController {
+
+    @GetMapping("about")
+    public String about(){
+        return "about";
+    }
+
+    @GetMapping("service")
+    public String service(){
+        return "service";
+    }
 }
